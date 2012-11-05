61adff1cb4a53bd298fe6ccf18529e2e7d9ba2f1
Biagio Grimaldi <biagio.grimaldi@abstract.it>
2012-11-05 17:12:48 +0100

    minor fix

--- src/vas.chances/vas/chances/urls.py
+++ src/vas.chances/vas/chances/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 from django.contrib.sitemaps import *
 from django.conf import settings
+from django.contrib import admin
 from django.conf.urls.defaults import patterns
 
 import logging
@@ -9,21 +10,38 @@ import logging
 log = logging.getLogger('vaschances.urls')
 
 
-extrapatterns = patterns('')
+admin.autodiscover()
+
+urlpatterns = getattr(settings, 'URLS', [])
+
+adminpatterns = patterns('',
+     (r'^admin/', include(admin.site.urls)),
+)
+
+
+if 'django.contrib.admin' in getattr(settings, 'INSTALLED_APPS', []):
+    if urlpatterns:
+        urlpatterns += adminpatterns
+    else:
+        urlpatterns = adminpatterns
 
 #The following is used to serve up local media files like images
 if getattr(settings, 'LOCAL_DEV', False):
     log.debug("Adding local serving of static files at: %s", settings.STATIC_ROOT)
     baseurlregex = r'^'+settings.STATIC_URL[1:]+'(?P<path>.*)$'
-    extrapatterns += patterns('',
+    urlpatterns += patterns('',
         (baseurlregex, 'django.views.static.serve',
         {'document_root':  settings.STATIC_ROOT, 'show_indexes': True})
     )
     log.debug("Adding local serving of media files at: %s", settings.MEDIA_ROOT)
     baseurlregex = r'^'+settings.MEDIA_URL[1:]+'(.*)$'
-    extrapatterns += patterns('',
+    urlpatterns += patterns('',
         (baseurlregex, 'django.views.static.serve',
         {'document_root':  settings.MEDIA_ROOT, 'show_indexes': True})
     )
 
-urlpatterns = extrapatterns
+urlpatterns = urlpatterns + patterns('',
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^settings/', include('livesettings.urls')),
+    (r'^cache/', include('keyedcache.urls')),
+)
